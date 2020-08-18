1e11a36b7e9503a4abc5155e4f965dc6c6b56f36
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-18 19:19:26 +0200

    models created

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from family.models import Family
 
 # Create your models here.
+
+class Media(models.Model):
+    title = models.CharField(max_length=64)
+    upload_date = models.DateTimeField()
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='user_images/')
+
+class Gallery(models.Model):
+    name = models.CharField(max_length=64)
+    create_date = models.DateTimeField()
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
