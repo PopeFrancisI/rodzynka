12c1dbb66cc9d28196b710d406f034b8a47d739e
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-19 11:22:38 +0200

    changed name from main_page to family_main everywhere

--- Rodzynka/urls.py
+++ Rodzynka/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from family.views import FamilyMainView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^family/(?P<slug>[a-z\d-]+)/$', FamilyMainView.as_view(), name='family_main'),
 ]
