e95e3c91cbc66855e0cc696eccc8247bd7c7c64d
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-19 10:24:20 +0200

    added family models correctly this time

--- family/models.py
+++ family/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Family(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    last_name = models.CharField(max_length=64)
+    users = models.ManyToManyField(User)
