cc21ff1ec0eec23b4bc2b67b7811389f0b23f814
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-19 11:21:58 +0200

    created FamilyMainView with basic implementation

--- family/views.py
+++ family/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class FamilyMainView(View):
+
+    def get(self, request):
+        context = {'testo': 'test context'}
+        return render(request, 'family_main.html', context)
