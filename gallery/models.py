cdcf1309d0c44d971cf63b68fe49f7e38447c86c
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-20 22:55:40 +0200

    early version of galleries page

--- gallery/models.py
+++ gallery/models.py
@@ -2,15 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from family.models import Family
 
+
 # Create your models here.
+class Gallery(models.Model):
+    name = models.CharField(max_length=64)
+    create_date = models.DateTimeField()
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+
 
 class Media(models.Model):
     title = models.CharField(max_length=64)
     upload_date = models.DateTimeField()
     uploader = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='user_images/')
+    galleries = models.ManyToManyField(Gallery)
 
-class Gallery(models.Model):
-    name = models.CharField(max_length=64)
-    create_date = models.DateTimeField()
-    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+
--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from family.models import Family
+from gallery.models import Gallery
 
 
 # Create your views here.
 class GalleryPickView(View):
 
     def get(self, request, family_slug):
-        return render(request, 'gallery_pick.html')
+        family = Family.objects.get(slug=family_slug)
+        galleries = family.gallery_set.all()
+        galleries_with_covers = []
+        for gallery in galleries:
+            galleries_with_covers.append((gallery, gallery.media_set.first()))
+        context = {'family': family, 'galleries': galleries_with_covers}
+
+        return render(request, 'gallery_pick.html', context)
