9d927fbdb97f9c5ca71944a71b2f3260943ec8a6
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-20 21:22:15 +0200

    early version of gallery template

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class GalleryPickView(View):
+
+    def get(self, request, family_slug):
+        return render(request, 'gallery_pick.html', {'family_id': request.session['active_family']})
