9dfa6ef0df4d796837b2366272b1c4fc65143881
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-20 16:11:20 +0200

    registration form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class RegistrationView(View):
+
+    def post(self, request):
+        form = UserCreationForm()
+        return render(request, 'registration.html', {'form': form})
