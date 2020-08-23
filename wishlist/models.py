4f0879d3943d2dae97fa430fc388abd77f11fd97
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-23 19:41:18 +0200

    wish description field/column is no longer required

--- wishlist/models.py
+++ wishlist/models.py
@@ -6,7 +6,7 @@ from family.models import Family
 
 class Wish(models.Model):
     title = models.CharField(max_length=64)
-    description = models.TextField()
+    description = models.TextField(null=True)
     is_important = models.BooleanField(default=False)
     family = models.ForeignKey(Family, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
