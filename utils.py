8c2517538b4393194c6570865a09f2597aa438c7
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-23 19:26:36 +0200

    cleared utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,1 @@
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
 
-
-def redirect_to_error_page(issue):
-    return redirect(reverse_lazy('something_gone_wrong'),
-                    args=(issue, ))
