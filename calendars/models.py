7faf60b9b96a2d5bd27fa591631a5b0ca285f86b
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-08-24 19:26:53 +0200

    calendar models

--- calendars/models.py
+++ calendars/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from family.models import Family
+
+
+class Calendar(models.Model):
+    name = models.CharField(max_length=64)
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(default='')
+    is_important = models.BooleanField(default=False)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
