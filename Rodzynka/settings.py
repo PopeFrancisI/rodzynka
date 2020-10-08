8c8107d247dde1f244225ff0cf47a563071f2d9a
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-10-08 19:47:51 +0200

    bootstrap and index page style

--- Rodzynka/settings.py
+++ Rodzynka/settings.py
@@ -108,12 +108,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
-
 try:
     from Rodzynka.local_settings import DATABASES
 except ModuleNotFoundError:
@@ -128,6 +122,11 @@ LOGOUT_REDIRECT_URL = 'index'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
