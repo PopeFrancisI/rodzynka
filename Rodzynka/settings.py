34c6464abd16a1870acd4ecf155c690977194727
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-10-27 20:44:55 +0100

    static root

--- Rodzynka/settings.py
+++ Rodzynka/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = 'static'
 
 WHITENOISE_USE_FINDERS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
