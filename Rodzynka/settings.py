3a9a794e9f911422d3718e40a98246f18ba1ba43
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-10-28 18:04:03 +0100

    another AWS fix

--- Rodzynka/settings.py
+++ Rodzynka/settings.py
@@ -118,18 +118,19 @@ LOGIN_REDIRECT_URL = 'family_pick'
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'index'
 
+AWS_DEFAULT_ACL = None
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'rodzynka.storage_backends.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media'
+MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
